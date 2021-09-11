22d416ee8dde341f9fabf110dc82f37faa585989
fyardlest <fyardlest1@gmail.com>
2021-09-11 14:36:44 -0400

    Adding multer
    Configure multer to save the image

--- src/routes/api/resize-images.ts
+++ src/routes/api/resize-images.ts
@@ -1,30 +1,66 @@
 import express, { Request, Response, Router } from "express";
 import path from "path";
 import sharp from "sharp";
-import { getAccessToFiles, getFilesName } from "../../utils/utilities";
+import multer from "multer"
+import { getFilesName } from "../../utils/utilities";
 const resizeImages: Router = Router();
 const app = express();
 
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValidType = FILE_TYPE_MAP[file.mimetype];
+    let uploadErr = new Error('the image type is invalid');
+    if (isValidType) {
+      uploadErr = null;
+    }
+    cb(uploadErr, "public/uploads");
+  },
+  filename: function (req, file, cb) {    
+    const fileName = file.originalname.replace(' ', '-');
+    const extention = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${fileName}-${Date.now()}.${extention}`);
+  },
+});
+
+const uploadImage = multer({ storage: storage });
+
 // access the images
-const importImages = path.resolve(__dirname, "images");
-// add the output file
-const exportImages = path.resolve(__dirname, "uploads");
+// const importImages = path.resolve(__dirname, "images");
+//localhost:5000/api/v1/resizeimages?filename=e-commerce-sales&width=200&height=200
 
 // allowing our application to parse json input
-app.use(express.json());
+http: app.use(express.json());
 
-resizeImages.get("/", (req: Request, res: Response) => {
+resizeImages.get("/", uploadImage.single('image'), async (req: Request, res: Response) => {
+  const file = req.file
+  if (!file) {
+    res.status(400).send('There is no file in the request')
+  }
+
+  const fileName = req.file.filename
+  if (!fileName) {
+    res.status(400).send("The filename does not exist");
+  }
+
+  const basePath: string = `${req.protocol}://${req.get('host')}/public/uploads`
+  
   const resizeImage = async (
     imageName: string,
     width: number | null,
     height: number | null
   ): Promise<string> => {
-    const resizedImageName = getFilesName(imageName, width, height);
+    const resizedImageName: string = getFilesName(imageName, width, height);
     try {
-      await sharp(`${importImages}${imageName}.jpg`)
+      await sharp(`${fileName}${imageName}.jpg`)
         .resize(width, height)
         .jpeg()
-        .toFile(`${importImages}${resizedImageName}`);      
+        .toFile(`${fileName}${basePath}`);      
     } catch (err) {
       res.status(400).json({ error: err});
     }      
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import images from './api/resize-images';
+import resizeImages from "./api/resize-images";
 const routes = express.Router();
 
 routes.get(`/`, (req, res) => {
 	res.send('main api route')
 })
 
-routes.use(`/resizeimages`, images);
+routes.use(`/resizeimages`, resizeImages);
 
 export default routes
